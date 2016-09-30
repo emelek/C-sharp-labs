b69a6d39f77d747a4c1afd8ca2f197ca4d988066
emelek <miha756_777@mail.ru>
2016-09-30 03:17:59 +0300

    Lab1,c#

---
+++ Lab1_gotovo/Lab1_gotovo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student a = new Student() { name = "Bob", age = 13, sex = true, colourHair = "Black" };
+            Student b = new Student() { name = "Mayson", age = 12, sex = true, colourHair = "Red" };
+            Student c = new Student() { name = "Marta", age = 14, sex = false, colourHair = "Brown" };
+            
+            a.Add(b);
+            Console.WriteLine("{0}\n{1} years per person\n{2} and {3}\n{4}", a.name,a.age/2,a.sex,b.sex,a.colourHair);
+            
+            a.Add(c);
+            Console.WriteLine();
+            Console.WriteLine("{0}\n{1} years per person\n{2} and {3} and {4}\n{5}", a.name, a.age / 3, a.sex, b.sex, c.sex, a.colourHair);
+            
+            Console.Read();
+        }
+    }
+}
---
+++ Lab1_gotovo/Lab1_gotovo/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Student
+    {
+        public string name { get; set; }
+        public double age { get; set; }
+        public bool sex { get; set; }
+        public string colourHair { get; set; }
+
+        public void Add(Student x)
+        {
+            age += x.age;
+            name =name + " and " + x.name;
+            colourHair = colourHair + " and " + x.colourHair;
+        }
+    }
+}
